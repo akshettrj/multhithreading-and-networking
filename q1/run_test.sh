f181b43b29bdad2ddf00838f9b697e1cc4687f76
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2021-11-22 02:22:55 +0530

    will start from scratch

--- q1/run_test.sh
+++ q1/run_test.sh
@@ -10,6 +10,8 @@ echo ""
 for test in ./tests/in/*.in
 do
     echo "$fg_bold[green]Running file $test"$reset_color
-    ./Q1 < $test
+    echo "$fg_bold[yellow]========================================================="$reset_color
+    ./Q1 < $test && echo "$fg_bold[green]Test $test passed"$reset_color || echo "$fg_bold[red]Test $test failed"$reset_color
+    echo "$fg_bold[yellow]========================================================="$reset_color
     echo ""
 done
